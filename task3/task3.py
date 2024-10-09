743a730432a2dcb127f5a6462b439b23a96202cd
GalinaShamordina <gmoskaleva@gmail.com>
2024-10-09 10:30:43 +0300

    Update task3.py

--- task3/task3.py
+++ task3/task3.py
@@ -1,41 +1,40 @@
 import json
 import sys
 
-def read_json(filename):
-    with open(filename, 'r') as file:
+def load_json(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
         return json.load(file)
-
 def write_json(filename, data):
-    with open(filename, 'w') as file:
-        json.dump(data, file, indent=4)
-
+    with open(filename, 'w',encoding='utf-8' ) as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
 def fill_report(tests, values):
-    if isinstance(tests, list):
-        for test in tests:
-            if isinstance(test, dict):
-                test_id = test.get('id')
-                if test_id in values:
-                    test['value'] = values[test_id]
-                else:
-                    test['value'] = None  
-            if 'tests' in test:
-                fill_report(test['tests'], values)
+    value_map = {item['id']: item['value'] for item in values['values']}
+    if isinstance(tests, dict):
+        for key, value in tests.items():
+            if key == 'id' and value in value_map:
+                tests['value'] = value_map[value]
+            else:
+                fill_report(value, values)
+    elif isinstance(tests, list):
+        
+        for item in tests:
+            fill_report(item, values)
 
 def main():
     if len(sys.argv) != 4:
-        print("Использование: python script.py <path_to_values.json> <path_to_tests.json> <path_to_report.json>")
+        print("Usage: python script.py <values.json> <tests.json> <report.json>")
         return
-
+    
     values_file = sys.argv[1]
     tests_file = sys.argv[2]
     report_file = sys.argv[3]
-
-    values = read_json(values_file)
-    tests = read_json(tests_file)
-
+    
+    values = load_json(values_file)
+    tests = load_json(tests_file)
+    
     fill_report(tests, values)
 
     write_json(report_file, tests)
-
+    
 if __name__ == "__main__":
     main()
